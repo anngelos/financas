65570767a3d4dd3fd78b3107f62a7b3ff544c064
Ângelo Cardoso <hoanngelos@gmail.com>
2023-12-08 15:48:49 -0300

    dropdown added to the user's name and logout option working.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="main">
-    <h1 class="dash-title">~~ DASHBOARD ~~</h1>
-    <img class="img"
-      :src="user.image ? require(`../../backend/public/images/users/${this.user.image}`) : require(`../assets/profilepic.jpg`)">
-    <h1><b>Name:</b> {{ user.name }}</h1>
-    <h1><b>Nickname:</b> {{ user.nickname }}</h1>
-    <button type="button" class="block w-60 bg-[#527853] mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
-      @click="logUserOut()">Sair</button>
-  </div>
+  <header class="header sticky top-0 bg-[#527853] shadow-md flex items-center justify-between px-8 py-02">
+    <h1 class="w-3/12"></h1>
+    <nav class="nav font-semibold text-lg">
+      <ul class="flex items-center">
+        <li class="p-4 border-b-2 border-white border-opacity-0 text-white hover:border-opacity-100 hover:text-white duration-200 cursor-pointer">
+          <a href="">Dívidas</a>
+        </li>
+        <li class="p-4 border-b-2 border-white border-opacity-0 text-white hover:border-opacity-100 hover:text-white duration-200 cursor-pointer">
+          <a href="">Criar dívida</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="w-3/12 flex justify-center items-center ang">
+      <div class="menu-item" @click="isOpen = !isOpen">
+        <span class="cursor-pointer text-white mr-2 font-bold">{{ user.name }}</span>
+          <transition name="fade" appear>
+            <div class="sub-menu" v-if="isOpen">
+              <div class="menu-item">
+                <ul>
+                  <li class="dditem">Editar Perfil</li>
+                  <li class="dditem">
+                    <button type="button" @click="logUserOut()">Sair</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </transition>
+      </div>
+      <img class="w-10 h-10 p-1 rounded-full user" :src="user.image ? require(`../../backend/public/images/users/${this.user.image}`) : require(`../assets/profilepic.jpg`)">
+    </div>
+  </header>
 </template>
 
 <script>
@@ -18,6 +40,7 @@ export default {
   data() {
     return {
       user: {},
+      isOpen: false
     }
   },
   methods: {
@@ -47,21 +70,35 @@ export default {
 </script>
 
 <style>
-.main {
-  margin: 400px auto;
-  width: auto;
-  height: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.dash-title {
-  font-size: 28px;
-}
-
 .img {
   width: 10%;
+}
+
+.menu-item .sub-menu {
+  border: 1px solid #ccc;
+  position: absolute;
+  background-color: white;
+  top: calc(38% + 18px);
+  left: 85%;
+  transform: translateX(-50%);
+  width: 100px;
+}
+
+.dditem {
+  margin-top: 5px;
+}
+
+.dditem:hover {
+  cursor: pointer;
+  background-color: #527853;
+  color: white;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s ease-out;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
